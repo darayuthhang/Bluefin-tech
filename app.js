d1735c62edebba516b2a9a98a7d5959053c5ba27
Darayuth Hang <darayuth@bluefintechpartners.com>
2021-05-19 14:52:05 -0400

    cor

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ app.use(express.urlencoded({
   extended: true
 }));
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
-app.use(function (req, res, next) {	
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
-    res.setHeader('Access-Control-Allow-Credentials', true);    
-    next();
-});
+// app.use(function (req, res, next) {	
+//     res.setHeader('Access-Control-Allow-Origin', "*");    
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
+//     res.setHeader('Access-Control-Allow-Credentials', true);    
+//     next();
+// });
 
 app.use(compression());
 
