4d1ca89bbf573adb41a89da6da259c76036907c6
Darayuth Hang <darayuth@bluefintechpartners.com>
2021-05-19 14:36:57 -0400

    up

--- app.js
+++ app.js
@@ -23,11 +23,12 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
+app.use(function (req, res, next) {	
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
+    res.setHeader('Access-Control-Allow-Credentials', true);    
+    next();
 });
 
 app.use(compression());
