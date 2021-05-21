cd42670ca92b7c545b31e4105377377386fb850f
Darayuth Hang <darayuth@bluefintechpartners.com>
2021-05-21 12:42:47 -0400

    update cors

--- app.js
+++ app.js
@@ -23,18 +23,18 @@ app.use(express.json());
 // app.use(express.urlencoded({
 //   extended: true
 // }));
-// app.use(cors({
-//   credentials: true,
-//   origin: "http://localhost:3001"
-// }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:3000"
+}));
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 //allow OPTIONS on all resources
 // app.use(function (req, res, next) {	
